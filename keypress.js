1d8c364752dbd4d585cd8aa33a0fe218de7f76d3
Nick Gilbert <nickgilbert2@gmail.com>
2018-04-26 12:09:02 -0700

    Added match and close match boxes
    The tab complete will post exact matches in one box, and substring in
    the other

--- keypress.js
+++ keypress.js
@@ -1,36 +1,16 @@
 var keyCodes = {
-  0 : "That key has no keycode",
-  3 : "break",
   8 : "backspace / delete",
-  9 : "tab",
-  12 : 'clear',
   13 : "enter",
-  16 : "shift",
-  17 : "ctrl",
-  18 : "alt",
-  19 : "pause/break",
-  20 : "caps lock",
-  21 : "hangul",
-  25 : "hanja",
+  //16 : "shift",
+  //17 : "ctrl",
+  //18 : "alt",
   27 : "escape",
-  28 : "conversion",
-  29 : "non-conversion",
-  32 : "spacebar",
-  33 : "page up",
-  34 : "page down",
-  35 : "end",
-  36 : "home",
-  37 : "left arrow",
-  38 : "up arrow",
+  //32 : "spacebar",
+  32 : " ",
+  //37 : "left arrow",
+  //38 : "up arrow",
   39 : "right arrow",
-  40 : "down arrow",
-  41 : "select",
-  42 : "print",
-  43 : "execute",
-  44 : "Print Screen",
-  45 : "insert",
-  46 : "delete",
-  47 : "help",
+  //40 : "down arrow",
   48 : "0",
   49 : "1",
   50 : "2",
@@ -42,11 +22,6 @@ var keyCodes = {
   56 : "8",
   57 : "9",
   58 : ":",
-  59 : "semicolon (firefox), equals",
-  60 : "<",
-  61 : "equals (firefox)",
-  63 : "ß",
-  64 : "@ (firefox)",
   65 : "a",
   66 : "b",
   67 : "c",
@@ -73,102 +48,33 @@ var keyCodes = {
   88 : "x",
   89 : "y",
   90 : "z",
-  91 : "Windows Key / Left ⌘ / Chromebook Search key",
-  92 : "right window key",
-  93 : "Windows Menu / Right ⌘",
-  95: "sleep",
-  96 : "numpad 0",
-  97 : "numpad 1",
-  98 : "numpad 2",
-  99 : "numpad 3",
-  100 : "numpad 4",
-  101 : "numpad 5",
-  102 : "numpad 6",
-  103 : "numpad 7",
-  104 : "numpad 8",
-  105 : "numpad 9",
-  106 : "multiply",
-  107 : "add",
-  108 : "numpad period (firefox)",
-  109 : "subtract",
-  110 : "decimal point",
-  111 : "divide",
-  112 : "f1",
-  113 : "f2",
-  114 : "f3",
-  115 : "f4",
-  116 : "f5",
-  117 : "f6",
-  118 : "f7",
-  119 : "f8",
-  120 : "f9",
-  121 : "f10",
-  122 : "f11",
-  123 : "f12",
-  124 : "f13",
-  125 : "f14",
-  126 : "f15",
-  127 : "f16",
-  128 : "f17",
-  129 : "f18",
-  130 : "f19",
-  131 : "f20",
-  132 : "f21",
-  133 : "f22",
-  134 : "f23",
-  135 : "f24",
-  144 : "num lock",
-  145 : "scroll lock",
+  //96 : "numpad 0",
+  //97 : "numpad 1",
+  //98 : "numpad 2",
+  //99 : "numpad 3",
+  //100 : "numpad 4",
+  //101 : "numpad 5",
+  //102 : "numpad 6",
+  //103 : "numpad 7",
+  //104 : "numpad 8",
+  //105 : "numpad 9",
+  96 : "0",
+  97 : "1",
+  98 : "2",
+  99 : "3",
+  100 : "4",
+  101 : "5",
+  102 : "6",
+  103 : "7",
+  104 : "8",
+  105 : "9",
   160 : "^",
   161 : '!',
   163 : "#",
   164 : '$',
-  165 : 'ù',
-  166 : "page backward",
-  167 : "page forward",
-  168 : "refresh",
-  169 : "closing paren (AZERTY)",
   170 : '*',
-  171 : "~ + * key",
-  172 : "home key",
-  173 : "minus (firefox), mute/unmute",
-  174 : "decrease volume level",
-  175 : "increase volume level",
-  176 : "next",
-  177 : "previous",
-  178 : "stop",
-  179 : "play/pause",
-  180 : "e-mail",
-  181 : "mute/unmute (firefox)",
-  182 : "decrease volume level (firefox)",
-  183 : "increase volume level (firefox)",
-  186 : "semi-colon / ñ",
-  187 : "equal sign",
-  188 : "comma",
-  189 : "dash",
-  190 : "period",
-  191 : "forward slash / ç",
-  192 : "grave accent / ñ / æ / ö",
-  193 : "?, / or °",
-  194 : "numpad period (chrome)",
-  219 : "open bracket",
-  220 : "back slash",
-  221 : "close bracket / å",
-  222 : "single quote / ø / ä",
-  223 : "`",
-  224 : "left or right ⌘ key (firefox)",
-  225 : "altgr",
-  226 : "< /git >, left back slash",
-  230 : "GNOME Compose Key",
-  231 : "ç",
-  233 : "XF86Forward",
-  234 : "XF86Back",
-  235 : "non-conversion",
-  240 : "alphanumeric",
-  242 : "hiragana/katakana",
-  243 : "half-width/full-width",
-  244 : "kanji",
-  255 : "toggle touchpad"
+  //193 : "?, / or °",
+  193 : "?",
 };
 
 // Dictionary of destinations, matched by command string
@@ -185,7 +91,12 @@ var dests = {
   "333syl"  : "http://web.cecs.pdx.edu/~markem/CS333/syllabi/2018-02-006.pdf",
   "333gui"  : "http://web.cecs.pdx.edu/~markem/CS333/handouts/guide",
   "ostep"   : "http://pages.cs.wisc.edu/~remzi/OSTEP/",
-  "xv6"     : "http://web.cecs.pdx.edu/~markem/CS333/xv6/xv6-book-rev8.pdf"
+  "xv6"     : "http://web.cecs.pdx.edu/~markem/CS333/xv6/xv6-book-rev8.pdf",
+  "mail"    : "https://mail.google.com/mail/u/0/#inbox",
+  "cal"     : "https://calendar.google.com/calendar/r?tab=mc",
+  "drive"   : "https://drive.google.com/drive/my-drive",
+  "shrltx"  : "https://www.sharelatex.com/project",
+  "lnkdin"  : "https://www.linkedin.com/"
 };
 
 var destshort = {
@@ -201,7 +112,12 @@ var destshort = {
   "333syl"  : "CS333 Syllabus",
   "333gui"  : "CS333 Survival Guide",
   "ostep"   : "OSTEP Book",
-  "xv6"     : "XV6 Book"
+  "xv6"     : "XV6 Book",
+  "mail"    : "Gmail",
+  "cal"     : "Google Calendar",
+  "drive"   : "Google Drive",
+  "shrltx"  : "ShareLaTeX",
+  "lnkdin"  : "LinkedIn"
 };
 
 var counter = 0;
@@ -214,13 +130,16 @@ document.onkeydown = function(evt) {
 
   var i;
   var prefix = "";
-  var postfix = "";
+  var match = "";
+  var close = "";
 
   if (cmds == 1){
     prefix = "Execute: ";
   }
-
-  if (keyCodes[evt.keyCode] == "escape") { // escape => start entering a command
+  if (keyCodes[evt.keyCode] == null) {
+    return;
+  }
+  else if (keyCodes[evt.keyCode] == "escape") { // escape => start entering a command
     if(cmds == 1){ // If we were already in command mode, turn it off
       cmds = 0;
       prefix = "";
@@ -270,13 +189,15 @@ document.onkeydown = function(evt) {
     if (out.length > 0) { // If we have a length in the string, try and find completes
       for (var key in dests) {
         if (key.indexOf(out) == 0) { // if we match from start, say so
-          postfix += " (" + key + " : " + destshort[key] + ") ";
+          match += " (" + key + " : " + destshort[key] + ") ";
         }
         else if (key.includes(out)) { // if we match at all, also say so
-          postfix += " ~ (" + key + " : " + destshort[key] + ") ";
+          close += " ~ (" + key + " : " + destshort[key] + ") ";
         }
       }
     }
   }
-  document.getElementById("keys").textContent=prefix + out + postfix;
+  document.getElementById("keys").textContent=prefix + out; //+ match;
+  document.getElementById("match").textContent=match;
+  document.getElementById("close").textContent=close;
 };
